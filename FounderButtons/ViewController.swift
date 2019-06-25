952e775fdf597cbe3328e93882ba5cf0be62f4d8
Cade Kelly <cademb223@gmail.com>
2019-06-25 11:31:21 -0700

    FoundersButtons v.1

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topTextBox: UITextField!
+    
+    @IBOutlet weak var bottomTextBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func googleButton(_ sender: Any) {
+        topTextBox.text = "Larry Page"
+        bottomTextBox.text = "Sergey Brin"
+    }
+    @IBAction func rtrButton(_ sender: Any) {
+        topTextBox.text = "Jennifer Hyman"
+        bottomTextBox.text = "Jenny Fleis"
+    }
+    @IBAction func clearButton(_ sender: Any) {
+        topTextBox.text = " "
+        bottomTextBox.text = " "
+    }
 }
 
